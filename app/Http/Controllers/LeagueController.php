6a1f84ead0a73f09936606abfdcaf38c7d257c34
Nate <jeditendencies35@gmail.com>
2023-11-22 13:53:48 -0500

    continuing to build out and style league gameday cards

--- app/Http/Controllers/LeagueController.php
+++ app/Http/Controllers/LeagueController.php
@@ -11,13 +11,14 @@ class LeagueController extends Controller
   public function index()
   {
     $dailyGames = [];
-    $currentDate = Carbon::now()->toDateString();
+    $today = Carbon::now();
+    $currentDate = Carbon::create($today)->toFormattedDateString();
     $weeklyGames = Http::get('https://api-web.nhle.com/v1/schedule/now')['gameWeek'];
     $currentStandings = Http::get('https://api-web.nhle.com/v1/standings/now')['standings'];
     $sortedStandingsByName = collect($currentStandings)->sortBy('teamName');
     // dd($weeklyGames);
     for ($i = 0; $i < count($weeklyGames); $i++) {
-      if ($weeklyGames[$i]['date'] === $currentDate) {
+      if ($weeklyGames[$i]['date'] === $today->toDateString()) {
         $dailyGames[] = $weeklyGames[$i]['games'];
       }
     }
