fadcbc991efda792782bfb1120214a1b406aff96
Nate <jeditendencies35@gmail.com>
2023-11-22 22:01:43 -0500

    whole app just started timing out for no reason

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ApiController extends Controller
+{
+  public static function getLinescores()
+  {
+    $linescores = Http::get('https://api-web.nhle.com/v1/score/now')['games'];
+    return $linescores;
+  }
+
+  public static function getWeeklyGames()
+  {
+    $weeklyGames = Http::get('https://api-web.nhle.com/v1/schedule/now')['gameWeek'];
+    return $weeklyGames;
+  }
+
+  public static function getCurrentStandings()
+  {
+    $currentStandings = Http::get('https://api-web.nhle.com/v1/standings/now')['standings'];
+    return $currentStandings;
+  }
+
+  public static function getBoxscores($id)
+  {
+    $boxscores = Http::get('https://api-web.nhle.com/v1/gamecenter/' . $id . '/boxscore')->json();
+    return $boxscores;
+  }
+}
--- app/Http/Controllers/LeagueController.php
+++ app/Http/Controllers/LeagueController.php
@@ -14,16 +14,16 @@ class LeagueController extends Controller
     $boxscores = [];
     $today = Carbon::today();
     $currentDate = Carbon::create($today)->toFormattedDateString();
-    $linescores = Http::get('https://api-web.nhle.com/v1/score/now')['games'];
-    $weeklyGames = Http::get('https://api-web.nhle.com/v1/schedule/now')['gameWeek'];
-    $currentStandings = Http::get('https://api-web.nhle.com/v1/standings/now')['standings'];
+    $linescores = ApiController::getLinescores();
+    $weeklyGames = ApiController::getWeeklyGames();
+    $currentStandings = ApiController::getCurrentStandings();
     $sortedStandingsByName = collect($currentStandings)->sortBy('teamName');
     // dd($weeklyGames);
     for ($i = 0; $i < count($weeklyGames); $i++) {
       if ($weeklyGames[$i]['date'] === $today->toDateString()) {
         $dailyGames[] = $weeklyGames[$i]['games'];
         for ($x = 0; $x < count($weeklyGames[$i]['games']); $x++) {
-          $boxscores[] = Http::get('https://api-web.nhle.com/v1/gamecenter/' . $weeklyGames[$i]['games'][$x]['id'] . '/boxscore')->json();
+          $boxscores[] = ApiController::getBoxscores($weeklyGames[$i]['games'][$x]['id']);
         }
       }
     }
