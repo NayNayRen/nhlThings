56f6922956090114b4a3f5116066fa3ed9908664
Nate <jeditendencies35@gmail.com>
2023-11-24 22:45:50 -0500

    turned 2 game code blocks into 1 include

---
+++ resources/views/includes/_gameCard.blade.php
@@ -0,0 +1,51 @@
+<div class="game-date-location">
+  <p class='game-date'> {{ $formattedGameDate }}
+  </p>
+  <p class='game-time'>{{ $formattedGameTime }} EST</p>
+  <p class="game-location">{{ $game['venue']['default'] }}</p>
+</div>
+{{-- AWAY TEAM --}}
+<div class="game-team-container">
+  <p>Away :</p>
+  @foreach ($allTeams as $team)
+    @if ($game['awayTeam']['abbrev'] === $team['teamAbbrev']['default'])
+      <p class='game-team-name'>
+        {{ $team['teamName']['default'] }}
+        <span class="game-team-logo">
+          <img src={{ $game['awayTeam']['logo'] }} alt='{{ $team['teamName']['default'] }}' width="100" height="100">
+        </span>
+      </p>
+      <p class='game-team-record'>{{ $team['wins'] }}-{{ $team['losses'] }}-{{ $team['otLosses'] }}
+      </p>
+    @endif
+  @endforeach
+</div>
+{{-- HOME TEAM --}}
+<div class="game-team-container">
+  <p>Home :</p>
+  @foreach ($allTeams as $team)
+    @if ($game['homeTeam']['abbrev'] === $team['teamAbbrev']['default'])
+      <p class='game-team-name'>
+        {{ $team['teamName']['default'] }}
+        <span class="game-team-logo home-team-logo">
+          <img src={{ $game['homeTeam']['logo'] }} alt='{{ $team['teamName']['default'] }}' width="100"
+            height="100">
+        </span>
+      </p>
+      <p class='game-team-record'>{{ $team['wins'] }}-{{ $team['losses'] }}-{{ $team['otLosses'] }}
+      </p>
+    @endif
+  @endforeach
+</div>
+{{-- GAME BROADCASTS --}}
+<p class="game-broadcast">
+  @if (count($game['tvBroadcasts']) < 1)
+    <span>Watch :</span>
+    <span>Check Listings</span>
+  @else
+    <span>Watch :</span>
+    @foreach ($game['tvBroadcasts'] as $tvBroadcast)
+      <span>{{ $tvBroadcast['network'] }}</span>
+    @endforeach
+  @endif
+</p>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,69 +24,20 @@
       @else
         <div class="league-regular-season-container">
           <ul class="league-regular-season owl-carousel owl-theme league-carousel">
-            @for ($i = 0; $i < count($dailyGames); $i++)
+            @foreach ($dailyGames as $key => $game)
               @php
-                $gameDateTime = Carbon\Carbon::create($dailyGames[$i]['startTimeUTC'])->tz('America/New_York');
+                $gameDateTime = Carbon\Carbon::create($game['startTimeUTC'])->tz('America/New_York');
                 $formattedGameDate = $gameDateTime->toFormattedDateString();
                 $formattedGameTime = $gameDateTime->format('h:i A');
               @endphp
               {{-- GAME CARDS --}}
               <li class="league-game-card">
-                <div class="game-date-location">
-                  <p class='game-date'> {{ $formattedGameDate }}
-                  </p>
-                  <p class='game-time'>{{ $formattedGameTime }} EST</p>
-                  <p class="game-location">{{ $dailyGames[$i]['venue']['default'] }}</p>
-                </div>
-                {{-- AWAY TEAM --}}
-                <div class="game-team-container">
-                  <p>Away :</p>
-                  @foreach ($allTeams as $team)
-                    @if ($dailyGames[$i]['awayTeam']['abbrev'] === $team['teamAbbrev']['default'])
-                      <p class='game-team-name'>
-                        {{ $team['teamName']['default'] }}
-                        <span class="game-team-logo">
-                          <img src={{ $dailyGames[$i]['awayTeam']['logo'] }} alt='{{ $team['teamName']['default'] }}'
-                            width="100" height="100">
-                        </span>
-                      </p>
-                      <p class='game-team-record'>{{ $team['wins'] }}-{{ $team['losses'] }}-{{ $team['otLosses'] }}</p>
-                    @endif
-                  @endforeach
-                </div>
-                {{-- HOME TEAM --}}
-                <div class="game-team-container">
-                  <p>Home :</p>
-                  @foreach ($allTeams as $team)
-                    @if ($dailyGames[$i]['homeTeam']['abbrev'] === $team['teamAbbrev']['default'])
-                      <p class='game-team-name'>
-                        {{ $team['teamName']['default'] }}
-                        <span class="game-team-logo">
-                          <img src={{ $dailyGames[$i]['homeTeam']['logo'] }} alt='{{ $team['teamName']['default'] }}'
-                            width="100" height="100">
-                        </span>
-                      </p>
-                      <p class='game-team-record'>{{ $team['wins'] }}-{{ $team['losses'] }}-{{ $team['otLosses'] }}
-                      </p>
-                    @endif
-                  @endforeach
-                </div>
-                {{-- GAME BROADCASTS --}}
-                <p class="game-broadcast">
-                  @if (count($dailyGames[$i]['tvBroadcasts']) < 1)
-                    <span>Watch :</span>
-                    <span>Check Listings</span>
-                  @else
-                    <span>Watch :</span>
-                    @foreach ($dailyGames[$i]['tvBroadcasts'] as $tvBroadcast)
-                      <span>{{ $tvBroadcast['network'] }}</span>
-                    @endforeach
-                  @endif
-                </p>
-                <span class='game-number'>{{ $i + 1 }} of
-                  {{ count($dailyGames) }}</span>
+                @include('includes._gameCard')
+                <span class='game-number'>
+                  {{ $key + 1 }} of {{ count($dailyGames) }}
+                </span>
               </li>
-            @endfor
+            @endforeach
           </ul>
           {{-- <p>{{ count($weeklyGames['gameWeek']) }}</p> --}}
           {{-- @foreach ($weeklyGames as $weeklyGame)
