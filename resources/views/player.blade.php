041519d949d5f950f05bcfcf3749b476824f568b
Nate <jeditendencies35@gmail.com>
2023-12-12 13:31:53 -0500

    added a little height to card container, player stats started

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -16,6 +16,48 @@
             height="100">
         </div>
       </div>
+      {{-- player data --}}
+      <div class="horizontal-scrolling-container">
+        <ul class="player-stats">
+          <li>
+            <h3 title="Season">Season</h3>
+            <h3 title="Games Played">GP</h3>
+            <h3 title="Goals">G</h3>
+            <h3 title="Assists">A</h3>
+            <h3 title="Points">P</h3>
+            <h3 title="Plus Minus">+/-</h3>
+            <h3 title="Penalty Minutes">PIM</h3>
+            <h3 title="Power Play Goals">PPG</h3>
+            <h3 title="Power Play Points">PPP</h3>
+            <h3 title="Short Handed Goals">SHG</h3>
+            <h3 title="Game Winning Goals">GWG</h3>
+            <h3 title="Over Time Goals">OTG</h3>
+            <h3 title="Shots">S</h3>
+            <h3 title="Shot %">S%</h3>
+            <h3 title="Time on Ice">TOI</h3>
+            <h3 title="Total TOI">TTOI</h3>
+          </li>
+
+          <li>
+            <p title="Regular Season">{{ $formattedSeason }}</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </main>
