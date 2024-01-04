5ee881e915b7f5c861017695ff730a72d3f9c424
Nate <jeditendencies35@gmail.com>
2024-01-03 22:53:50 -0500

    700 and under sticky is set to fixed in css too, think i got it

--- public/js/gameScript.js
+++ public/js/gameScript.js
@@ -5,26 +5,26 @@ function stickHeading() {
   if (document.documentElement.scrollTop > 90 && window.innerWidth > 1000) {
     stickyHeading.style.position = "fixed";
     stickyHeading.style.width = "calc(85% - 34px)";
-    stickyHeading.style.top = "98px";
-    scrollPoint.style.paddingTop = "140px";
+    stickyHeading.style.top = "95px";
+    scrollPoint.style.paddingTop = "135px";
   }
   else if (document.documentElement.scrollTop > 50 && window.innerWidth < 1000 &&
     window.innerWidth > 700) {
     stickyHeading.style.position = "fixed";
     stickyHeading.style.width = "calc(100% - 40px)";
-    stickyHeading.style.top = "98px";
+    stickyHeading.style.top = "95px";
     scrollPoint.style.paddingTop = "135px";
   }
-  else if (document.documentElement.scrollTop > 0 && window.innerWidth < 700 && window.innerWidth > 400) {
+  else if (window.innerWidth < 700 && window.innerWidth > 400) {
     stickyHeading.style.position = "fixed";
     stickyHeading.style.width = "100%";
     stickyHeading.style.top = "85px";
     scrollPoint.style.paddingTop = "140px";
   }
-  else if (document.documentElement.scrollTop > 0 && window.innerWidth <= 400) {
+  else if (window.innerWidth <= 400) {
     stickyHeading.style.position = "fixed";
     stickyHeading.style.width = "100%";
-    stickyHeading.style.top = "105px";
+    stickyHeading.style.top = "100px";
     scrollPoint.style.paddingTop = "130px";
   }
   else {
