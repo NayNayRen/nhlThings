20541264f5d2b8ae92b630b6bb56f6c80d83d500
Nate <jeditendencies35@gmail.com>
2024-03-15 22:43:08 -0400

    getting the shots worked out too

--- resources/views/includes/matchup/_matchup_game_shots.blade.php
+++ resources/views/includes/matchup/_matchup_game_shots.blade.php
@@ -1,25 +1,25 @@
 <li>
-  @if ($shots['period'] === 1)
+  @if ($shots['periodDescriptor']['number'] === 1)
     <p>{{ $shots['away'] }}</p>
-    <p>{{ $shots['period'] }}st Period</p>
+    <p>1st Period</p>
     <p>{{ $shots['home'] }}</p>
   @endif
-  @if ($shots['period'] === 2)
+  @if ($shots['periodDescriptor']['number'] === 2)
     <p>{{ $shots['away'] }}</p>
-    <p>{{ $shots['period'] }}nd Period</p>
+    <p>2nd Period</p>
     <p>{{ $shots['home'] }}</p>
   @endif
-  @if ($shots['period'] === 3)
+  @if ($shots['periodDescriptor']['number'] === 3)
     <p>{{ $shots['away'] }}</p>
-    <p>{{ $shots['period'] }}rd Period</p>
+    <p>3rd Period</p>
     <p>{{ $shots['home'] }}</p>
   @endif
-  @if ($shots['period'] === 4)
+  @if ($shots['periodDescriptor']['number'] === 4)
     <p>{{ $shots['away'] }}</p>
     <p>OT</p>
     <p>{{ $shots['home'] }}</p>
   @endif
-  @if ($shots['period'] >= 5)
+  @if ($shots['periodDescriptor']['number'] >= 5)
     <p></p>
   @endif
 </li>
